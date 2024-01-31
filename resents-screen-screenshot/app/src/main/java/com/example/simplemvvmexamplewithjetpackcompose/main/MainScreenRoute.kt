59f4989c03d6ae6837a039d6a15c9f87c7d5073c
project <whatminjacodes@gmail.com>
2024-01-31 14:24:08 +0200

    Deleted files

--- resents-screen-screenshot/app/src/main/java/com/example/simplemvvmexamplewithjetpackcompose/main/MainScreenRoute.kt
+++
@@ -1,8 +0,0 @@
-package com.example.simplemvvmexamplewithjetpackcompose.main
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun MainScreenRoute(messageFromStart: String) {
-    FirstScreen(messageFromStart = messageFromStart)
-}
