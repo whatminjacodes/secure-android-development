8dd5f327c72c839dcd04ffacb84d31b07f6be350
project <whatminjacodes@gmail.com>
2024-02-02 16:22:09 +0200

    Added fragment

---
+++ resents-screen-screenshot/app/src/main/java/com/example/recentsscreenscreenshot/insecure/InSecureFragment.kt
@@ -0,0 +1,32 @@
+package com.example.recentsscreenscreenshot.insecure
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+
+class InSecureFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            // Dispose of the Composition when the view's LifecycleOwner
+            // is destroyed
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                MaterialTheme {
+                    // In Compose world
+                    Text("Hello Compose!")
+                }
+            }
+        }
+    }
+}
